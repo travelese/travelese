a4aaadbd8600699802d57536b01a1b1ee140b988
Armin <armin.babaei@me.com>
2024-12-04 12:56:45 -0500

    DEV-27
    Added Expo and ai sdk for mobile

---
+++ apps/mobile/app/(tabs)/index.tsx
@@ -0,0 +1,61 @@
+import { generateAPIUrl } from "@/utils";
+import { useChat } from "@ai-sdk/react";
+import { fetch as expoFetch } from "expo/fetch";
+import { SafeAreaView, ScrollView, Text, TextInput, View } from "react-native";
+
+export default function TabOneScreen() {
+  const { messages, error, handleInputChange, input, handleSubmit } = useChat({
+    fetch: expoFetch as unknown as typeof globalThis.fetch,
+    api: generateAPIUrl("/api/chat"),
+    onError: (error) => console.error(error, "ERROR"),
+    maxSteps: 5,
+  });
+
+  if (error) return <Text>{error.message}</Text>;
+
+  return (
+    <SafeAreaView style={{ height: "100%" }}>
+      <View
+        style={{
+          height: "95%",
+          display: "flex",
+          flexDirection: "column",
+          paddingHorizontal: 8,
+        }}
+      >
+        <ScrollView style={{ flex: 1 }}>
+          {messages.map((m) => (
+            <View key={m.id} style={{ marginVertical: 8 }}>
+              <View>
+                <Text style={{ fontWeight: 700 }}>{m.role}</Text>
+                <Text>{m.content}</Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+
+        <View style={{ marginTop: 8 }}>
+          <TextInput
+            style={{ backgroundColor: "white", padding: 8 }}
+            placeholder="Say something..."
+            value={input}
+            onChange={(e) =>
+              handleInputChange({
+                ...e,
+                target: {
+                  ...e.target,
+                  value: e.nativeEvent.text,
+                },
+              } as unknown as React.ChangeEvent<HTMLInputElement>)
+            }
+            onSubmitEditing={(e) => {
+              handleSubmit(e);
+              e.preventDefault();
+            }}
+            autoFocus={true}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
