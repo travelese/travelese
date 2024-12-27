88c93ed16834e941d1c242dd97a94378f2fde6fa
Armin <armin.babaei@me.com>
2024-12-27 14:14:13 -0500

    DEV-35 (#88)
    https://linear.app/travelese/issue/DEV-35/transactions

--- apps/dashboard/src/actions/transactions/update-currency-action.ts
+++ apps/dashboard/src/actions/transactions/update-currency-action.ts
@@ -1,9 +1,8 @@
 "use server";
 
 import { LogEvents } from "@travelese/events/events";
-import { Jobs } from "@travelese/jobs";
 import { updateTeam } from "@travelese/supabase/mutations";
-import { auth, tasks } from "@trigger.dev/sdk/v3";
+import { updateBaseCurrency } from "jobs/tasks/transactions/update-base-currency";
 import { revalidatePath, revalidateTag } from "next/cache";
 import { z } from "zod";
 import { authActionClient } from "../safe-action";
@@ -23,6 +22,10 @@ export const updateCurrencyAction = authActionClient
   })
   .action(
     async ({ parsedInput: { baseCurrency }, ctx: { user, supabase } }) => {
+      if (!user.team_id) {
+        throw new Error("No team id");
+      }
+
       await updateTeam(supabase, {
         id: user.team_id,
         base_currency: baseCurrency,
@@ -31,22 +34,11 @@ export const updateCurrencyAction = authActionClient
       revalidateTag(`team_settings_${user.team_id}`);
       revalidatePath("/settings/accounts");
 
-      const publicToken = await auth.createPublicToken({
-        scopes: {
-          read: {
-            runs: true, // For testing - specify exact runs in production
-          },
-        },
-      });
-
-      const event = await tasks.trigger(Jobs.UPDATE_CURRENCY, {
-        baseCurrency,
+      const event = await updateBaseCurrency.trigger({
         teamId: user.team_id,
+        baseCurrency,
       });
 
-      return {
-        ...event,
-        publicToken,
-      };
+      return event;
     },
   );
