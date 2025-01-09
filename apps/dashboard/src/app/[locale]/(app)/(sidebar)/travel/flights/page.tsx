bd5f1f5fc7b8a7a5e2ddb912a335f32a61a2184a
Armin <armin.babaei@me.com>
2025-01-09 18:37:12 -0500

    DEV-38
    https://linear.app/travelese/issue/DEV-38/flights-page-layout

---
+++ apps/dashboard/src/app/[locale]/(app)/(sidebar)/travel/flights/page.tsx
@@ -0,0 +1,10 @@
+import { FlightSearch } from "@/components/forms/travel-flight-search-form"
+import { TravelSearchForm } from "@/components/forms/travel-search-form";
+
+export default function Flights() {
+  return (
+    <div>
+        <FlightSearch />
+    </div>
+  )
+}
